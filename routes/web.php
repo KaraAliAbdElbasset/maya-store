41b65718db15001038bad3ea54891720a3aca865
Abd Elbasset <karaliabdelbasset@outlook.com>
2021-05-10 17:41:02 +0100

    update : seeding route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 // for deployment
 Route::get('/artisan', [\App\Http\Controllers\WebsiteController::class,'artisan'])->name('artisan');
 Route::get('/cache', [\App\Http\Controllers\WebsiteController::class,'cache'])->name('cache');
-Route::get('/seed', [\App\Http\Controllers\WebsiteController::class,'seed'])->name('seed');
+Route::get('/seed', [\App\Http\Controllers\WebsiteController::class,'seeding'])->name('seed');
 
 
 /**********************************************************************************/
