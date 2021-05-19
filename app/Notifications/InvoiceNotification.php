e4c9e4db9862b1c54a85147d4918a4e0e31c7271
Abd Elbasset <karaliabdelbasset@outlook.com>
2021-05-20 00:53:02 +0100

    add invoice notification

---
+++ app/Notifications/InvoiceNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class InvoiceNotification extends Notification
+{
+    use Queueable;
+    public $data;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return MailMessage
+     */
+    public function toMail($notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('New Invoice Notification')
+            ->greeting('Hi '.$notifiable->name)
+            ->line('Hope this email finds you well! Please see attached invoice, Feel free to reach out if you have any questions.')
+            ->line('Thank you for using our application!')
+            ->attach(asset($this->data['path']), [
+                'mime' => 'application/pdf',
+            ]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
