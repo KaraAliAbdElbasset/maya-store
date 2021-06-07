f60408fb4f27c7dbc1e41bfb82c15573afd558f4
maya.belbali3@gmail.com <you@example.com>
2021-06-07 10:28:07 +0200

    pic produit

--- resources/views/layouts/partials/product_card.blade.php
+++ resources/views/layouts/partials/product_card.blade.php
@@ -2,7 +2,7 @@
 
     <div class="card product border-0">
 
-        <div class="product-image" style="aspect-ratio: 16 / 9; background: url('{{$p->image_url}}'); background-repeat: no-repeat; background-position: center; background-size: cover">
+        <div class=" meme_hauteur" style="aspect-ratio: 16 / 9; background: url('{{$p->image_url}}'); background-repeat: no-repeat; background-position: center; background-size: cover">
             @if($p->discount)
                 <span class="badge bg-orange rounded-2 product-discount">- {{$p->discount}}%</span>
             @endif
